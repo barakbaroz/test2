2e26baa84ee3465d2c5581a7e9726bb866e5604c
Barak <barozbarak@gmail.com>
2024-02-11 12:35:17 +0200

    Fit GisterStep component

--- client/src/components/Gister/GisterStep.jsx
+++ client/src/components/Gister/GisterStep.jsx
@@ -1,16 +1,11 @@
 import styled from "styled-components";
 import PropTypes from "prop-types";
 
-function GisterStep({ stepNumber, children, title, subTitle }) {
+export default function GisterStep({ children, title, ...props }) {
   return (
-    <StepContainer>
-      <InformationContainer>
-        <StepNumber>{stepNumber}</StepNumber>
-        <Text>
-          <Title>{title}</Title> <SubTitle>{subTitle}</SubTitle>
-        </Text>
-      </InformationContainer>
-      {children}
+    <StepContainer {...props}>
+      <Title>{title}</Title>
+      <WarpperChildren>{children}</WarpperChildren>
     </StepContainer>
   );
 }
@@ -18,49 +13,29 @@ function GisterStep({ stepNumber, children, title, subTitle }) {
 GisterStep.propTypes = {
   children: PropTypes.node,
   title: PropTypes.string,
-  subTitle: PropTypes.string,
-  stepNumber: PropTypes.string,
 };
-export default GisterStep;
 
 const StepContainer = styled.div`
   height: 100%;
   display: flex;
   flex-direction: column;
-  padding: 0 2rem;
+  border: solid 1px #dadada;
+  border-radius: 20px;
+  &.invalid {
+    border-color: var(--invalid);
+  }
 `;
 
-const InformationContainer = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  text-align: start;
-  margin-bottom: 56px;
-`;
-
-const StepNumber = styled.div`
-  font-size: 3.438rem;
-  font-weight: 300;
-  font-family: "Poppins";
-  color: #84a4fc;
+const WarpperChildren = styled.div`
+  padding-inline: 50px;
+  padding-block: 38px;
 `;
 
 const Title = styled.span`
   font-size: 1.5rem;
   font-weight: 500;
-`;
-
-const SubTitle = styled.span`
-  font-size: 1.25rem;
-  font-weight: 400;
-`;
-
-const Text = styled.div`
-  width: fit-content;
+  padding-inline: 50px;
   border-bottom: 1px solid #cecece;
-  padding-bottom: 1rem;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  gap: 14px;
+  padding-block-start: 1.875rem;
+  padding-block-end: 1.4375rem;
 `;
