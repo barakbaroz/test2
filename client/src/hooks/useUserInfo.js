8c8855d8ce52f5869ce215d81bcefd1b34ce3c9f
Barak <barozbarak@gmail.com>
2024-02-20 10:55:45 +0200

    Add "userInstructions" in useUser hook

--- client/src/hooks/useUserInfo.js
+++ client/src/hooks/useUserInfo.js
@@ -10,6 +10,15 @@ export default function useUserInfo() {
   const [userInfo, setUserInfo] = useState({ Case: {} });
   const navigate = useNavigate();
 
+  const getUserInstructions = (user) => {
+    const { AtrialFibrillation, HeartFailure } = user.Case;
+    if (AtrialFibrillation && HeartFailure)
+      return "Atrial-Fibrillation-Heart-Failure";
+    if (AtrialFibrillation) return "Atrial-Fibrillation";
+    if (HeartFailure) return "Heart-Failure";
+    return "none";
+  };
+
   const updateCase = (newData) => {
     setUserInfo((prev) => ({ ...prev, Case: { ...prev.Case, ...newData } }));
     if (newData.Avatar.gender) setGender(newData.Avatar.gender);
@@ -36,6 +45,7 @@ export default function useUserInfo() {
     axios
       .get("/api/user/getData")
       .then((res) => {
+        res.data.instructions = getUserInstructions(res.data);
         setLanguage(res.data.language);
         setGender(res.data.Case.Avatar.gender);
         setUserInfo(res.data);
