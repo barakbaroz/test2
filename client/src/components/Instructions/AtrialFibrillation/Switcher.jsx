5007b83a0713d61acb955bd0bf9c416add111395
Barak <barozbarak@gmail.com>
2024-02-28 13:12:27 +0200

    Build

--- client/src/components/Instructions/AtrialFibrillation/Switcher.jsx
+++ client/src/components/Instructions/AtrialFibrillation/Switcher.jsx
@@ -17,6 +17,9 @@ export default function Switcher({ procedure, setProcedure }) {
     });
   };
 
+  console.log(instructions);
+  console.log(sending);
+
   if (sending === "second") return <></>;
   if (instructions !== "atrial-fibrillation-heart-failure") return <></>;
 
