825d00dd35d88801a45667d87a2c263fa73dc495
Barak <barozbarak@gmail.com>
2024-02-28 10:16:23 +0200

    Create Switcher component

---
+++ client/src/components/Instructions/AtrialFibrillation/Switcher.jsx
@@ -0,0 +1,77 @@
+import styled, { css } from "styled-components";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { useContext } from "react";
+import { Translator } from "../../Translation";
+import { userContext } from "../../../providers/UserProvider";
+import PropTypes from "react";
+
+export default function Switcher({ procedure, setProcedure }) {
+  const { Case } = useContext(userContext);
+  const { sending } = useParams();
+  const { instructions } = Case;
+
+  const handleSelect = (procedure) => {
+    setProcedure(procedure);
+    axios.post("/api/users/userAction", {
+      type: `switch-to-${procedure}`,
+    });
+  };
+
+  if (sending === "second") return <></>;
+  if (instructions !== "atrial-fibrillation-heart-failure") return <></>;
+
+  return (
+    <Wrapper>
+      <ProcedureOption
+        selected={procedure === "atrial-fibrillation"}
+        onClick={() => handleSelect("atrial-fibrillation")}
+      >
+        <Translator>Video-Atrial-Fibrillation</Translator>
+      </ProcedureOption>
+      <ProcedureOption
+        selected={procedure === "heart-failure"}
+        onClick={() => handleSelect("heart-failure")}
+      >
+        <Translator>Video-Heart-Failure</Translator>
+      </ProcedureOption>
+    </Wrapper>
+  );
+}
+Switcher.propTypes = {
+  procedure: PropTypes.string,
+  setProcedure: PropTypes.func,
+};
+
+const Wrapper = styled.div`
+  margin-block-start: 35px;
+  margin-block-end: 27px;
+  display: flex;
+  align-items: center;
+  font-size: 0.938rem;
+  border-radius: 2rem;
+  background: #e3e8f6;
+  padding: 0.26rem 0.4rem;
+  margin-inline: 15px;
+  font-size: 1rem;
+`;
+
+const ProcedureOption = styled.div`
+  display: flex;
+  align-items: center;
+  border-radius: 2rem;
+  padding-inline: 10px;
+  padding-block: 0.5rem;
+  text-align: center;
+  width: 100%;
+  cursor: pointer;
+  background-color: #e3e8f6;
+  color: #7a9dfd;
+  ${({ selected }) =>
+    selected &&
+    css`
+      background-color: #84a4fc;
+      box-shadow: 0px 2px 6px #00000029;
+      color: white;
+    `}
+`;
