cffbcc5373efb02a41da02f479b7eff2b121e1da
tomer255 <tomer255255@gmail.com>
2024-05-01 11:09:06 +0300

    Fix update

--- client/src/hooks/useUserInfo.js
+++ client/src/hooks/useUserInfo.js
@@ -29,7 +29,7 @@ export default function useUserInfo() {
     setUserInfo((prev) => ({
       ...prev,
       Questionnaires: {
-        ...prev,
+        ...prev.Questionnaires,
         ...questionaireAnswers,
       },
     }));
